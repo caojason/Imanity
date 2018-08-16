5a420e2e5850f8d9b1d0d6ce6c2a7a6b1a8c01c1
unknown <caojason001@gmail.com>
2018-08-16 15:52:25 -0700

    Added allowed_hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bcj96-+h^8k^1t@-ygoxt^e^ntn^gj
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immanity.herokuapp.com']
 
 
 # Application definition
